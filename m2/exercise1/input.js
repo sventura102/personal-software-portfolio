c829f4d958ddd3467f39f3f2867b7f419177aa48
Stephanie <sventura102@gmail.com>
2024-05-11 16:47:51 -0700

    formatted w/ prettier

--- m2/exercise1/input.js
+++ m2/exercise1/input.js
@@ -1,20 +1,20 @@
-// Code exercise: write a function that takes input 
+// Code exercise: write a function that takes input
 function recordName() {
-    //get elements
-    let name = document.getElementById('name').value;
-    let id = document.getElementById('id_number').value;
+  //get elements
+  let name = document.getElementById("name").value;
+  let id = document.getElementById("id_number").value;
 
-    //create table for display
-    let tr = document.createElement('tr');
+  //create table for display
+  let tr = document.createElement("tr");
 
-    //create colums within the table
-    let col1 = tr.appendChild(document.createElement('td'));
-    let col2 = tr.appendChild(document.createElement('td'));
+  //create colums within the table
+  let col1 = tr.appendChild(document.createElement("td"));
+  let col2 = tr.appendChild(document.createElement("td"));
 
-    //assign data to the columns
-    col1.innerHTML = name;
-    col2.innerHTML = id;
-    
-    //display data in the HTML table
-    document.getElementById('table').appendChild(tr);
+  //assign data to the columns
+  col1.innerHTML = name;
+  col2.innerHTML = id;
+
+  //display data in the HTML table
+  document.getElementById("table").appendChild(tr);
 }
