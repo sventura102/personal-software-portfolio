4c666ee74cd4ef22f14a8f1181e13af2922a71f0
Stephanie <sventura102@gmail.com>
2024-05-20 20:06:39 -0700

    beginning stages of student gradebook

---
+++ m3/studentGradebook.java
@@ -0,0 +1,31 @@
+package m3;
+import java.util.HashMap;
+
+public class studentGradebook {
+    public class Student {
+        // setters
+        public int studentID;
+        public String studentName;
+        // Grades will be stored here!
+        public HashMap<String, Double> grades = new HashMap<String, Double>();
+
+        //Getter and Setters for the Student
+        public Student(int studentID, String studentName) {
+            this.studentID = studentID;
+            this.studentName = studentName;
+        }
+        //
+        public void displayInfo() {
+            System.out.println("Student ID: " + studentID);
+            System.out.println("Student Name: " + studentName);
+            System.out.println("Grades: " + grades);
+        }
+
+    }
+    // constructor to add students
+    public studentGradebook(int studentID, String studentName) {
+        Student student = new Student(studentID, studentName);
+
+        student.displayInfo();
+    }
+}
