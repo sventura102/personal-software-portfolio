b09331b923b3976f9a7f038fbbc97d9b35d20c45
Stephanie <sventura102@gmail.com>
2024-05-21 14:52:33 -0700

    added getters inside of the Student and Course classes

--- m3/studentGradebook.java
+++ m3/studentGradebook.java
@@ -8,8 +8,7 @@ public class studentGradebook {
         // setters
         int studentID;
         String studentName;
-        // Grades will be stored here!
-        HashMap<String, Double> grades = new HashMap<String, Double>();
+
 
         // Constructor for the Student
         public Student(int studentID, String studentName) {
@@ -21,36 +20,64 @@ public class studentGradebook {
         public void displayInfo() {
             System.out.println("Student ID: " + studentID);
             System.out.println("Student Name: " + studentName);
-            System.out.println("Grades: " + grades);
         }
 
+        //get Student ID
+        public int getstudentID() {
+            return studentID;
+        }
+        //get Student Name
+        public String getstudentName() {
+            return studentName;
+        }
     }
 
     public class Course {
         // variable for course name
-        String courseName;
+        public String courseName;
+        // Grades will be stored here!
+        public ArrayList<Double> grades;
 
         // implement constructor for course name
         public Course(String courseName) {
             this.courseName = courseName;
         }
+        // constructor for list
+        public void Grade(ArrayList<Double> grades) {
+            this.grades = grades;
+        }
 
-        // getter and setter for the course name
+        // getter and setter for the course name and grades
+        public String getCourseName() {
+            return courseName;
+        }
 
+        public ArrayList<Double> getGrades(){
+            return grades;
+        }
+        // function to add a grade into the array
+        public void addGrade(Double grade) {
+            grades.add(grade);
+        }
+        //function to get the grades, add them, and return the average
+        public void getAverageGrades() {
+
+        }
     }
 
     public class GradeBook {
-        // variable to store list of students
-        ArrayList<String> studentList = new ArrayList<String>();
+        // variable to store list of students with Id
+        HashMap<Integer,String> studentList = new HashMap<Integer,String>();
         
         // add student to gradebook
-        public void addStudent(int studentID, String studentName) {
-            Student student = new Student(studentID, studentName);
+        //public void addStudent(int studentID,String studentName) {
+        //    Student student = new Student(studentID, studentName);
+            //add to student List
+        //    studentList.put(studentID, studentName);
             
-            student.displayInfo();
-        }
+        //}
         // add grade for student in specific course
-        public void addStudentCourseGrade() {
+        public void addStudentCourseGrade(String studentName, String course, Double grade) {
 
         }
         // calculate average grade for student
@@ -61,6 +88,17 @@ public class studentGradebook {
         public void studentReportCards() {
 
         }
+    }
+    public static void main(String[] args) {
+        //Add Students
+
+        //Add Courses
+
+        //Add Grades
+
+        //Printing details
+
+        //Showing information in a separate file
     }
 
 }
