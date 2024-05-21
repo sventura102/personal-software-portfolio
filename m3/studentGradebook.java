c2aa81674eeeab136c764340590edaf1e8d23a41
Stephanie <sventura102@gmail.com>
2024-05-20 20:27:59 -0700

    removed public from student class variavles and created additional functions inside of GradeBook Class

--- m3/studentGradebook.java
+++ m3/studentGradebook.java
@@ -1,19 +1,22 @@
 package m3;
+
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class studentGradebook {
     public class Student {
         // setters
-        public int studentID;
-        public String studentName;
+        int studentID;
+        String studentName;
         // Grades will be stored here!
-        public HashMap<String, Double> grades = new HashMap<String, Double>();
+        HashMap<String, Double> grades = new HashMap<String, Double>();
 
-        //Getter for the Student
+        // Constructor for the Student
         public Student(int studentID, String studentName) {
             this.studentID = studentID;
             this.studentName = studentName;
         }
+
         // display student, will change for file integration
         public void displayInfo() {
             System.out.println("Student ID: " + studentID);
@@ -22,10 +25,42 @@ public class studentGradebook {
         }
 
     }
-    // constructor to add students
-    public studentGradebook(int studentID, String studentName) {
-        Student student = new Student(studentID, studentName);
 
-        student.displayInfo();
+    public class Course {
+        // variable for course name
+        String courseName;
+
+        // implement constructor for course name
+        public Course(String courseName) {
+            this.courseName = courseName;
+        }
+
+        // getter and setter for the course name
+
     }
+
+    public class GradeBook {
+        // variable to store list of students
+        ArrayList<String> studentList = new ArrayList<String>();
+        
+        // add student to gradebook
+        public void addStudent(int studentID, String studentName) {
+            Student student = new Student(studentID, studentName);
+            
+            student.displayInfo();
+        }
+        // add grade for student in specific course
+        public void addStudentCourseGrade() {
+
+        }
+        // calculate average grade for student
+        public void calculateGradeAverage() {
+
+        }
+        // generate report for all students and their grades
+        public void studentReportCards() {
+
+        }
+    }
+
 }
